9451c44d18d43627f669f774f5c2351e4ab692b8
omericoramos <omericoramos@gmail.com>
2024-12-13 14:32:25 -0300

    instalando e configurando biblioteca flowbite

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,29 +1,2 @@
 <img id="logo" class="block w-48 mx-6 fill-current text-gray-800 dark:text-gray-200"
     src="{{ asset('images/logo-light.png') }}" alt="Oikana Logo">
-
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const logo = document.getElementById('logo');
-
-        // Função para alternar a imagem da logo com base no tema
-        const updateLogo = () => {
-            if (document.documentElement.classList.contains('dark')) {
-                logo.src = "{{ asset('images/logo-dark.png') }}"; // Caminho da logo no tema dark
-            } else {
-                logo.src = "{{ asset('images/logo-light.png') }}"; // Caminho da logo no tema light
-            }
-        };
-
-        // Atualiza a logo ao carregar a página
-        updateLogo();
-
-        // Alterna a logo ao clicar no botão de tema (caso exista)
-        const themeToggle = document.getElementById('theme-toggle');
-        if (themeToggle) {
-            themeToggle.addEventListener('click', () => {
-                setTimeout(updateLogo,
-                100); // Pequeno delay para garantir que a classe dark foi alternada
-            });
-        }
-    });
-</script>
